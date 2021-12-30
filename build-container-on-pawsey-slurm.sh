1d473882c2fdc6cef4fe09e40d24a42f0ee273d0
IT-gMA <tiendatoan@gmail.com>
2021-12-30 23:47:57 +0800

    slurm script for building container

---
+++ build-container-on-pawsey-slurm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash  --login
+
+#SBATCH --partition=gpuq
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBTACH --account=interns2021026
+#SBATCH --export=NONE
+
+module load singularity
+
+time srun singularity remote login --tokenfile sylabs-token
+time srun singularity build -r yolov5_run_container.sif build-container-on-pawsey.def
