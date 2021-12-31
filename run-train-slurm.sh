35089ad0197e6bf8b75059809d412b4d1941eaf7
IT-gMA <tiendatoan@gmail.com>
2021-12-31 20:44:21 +0800

    update to run training script

--- run-train-slurm.sh
+++ run-train-slurm.sh
@@ -7,7 +7,6 @@
 #SBATCH --tasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBTACH --account=interns2021026
-#SBATCH --export=NONE
 #SBATCH --output=train_run.out
 
 module load cuda
@@ -18,6 +17,8 @@ export SINGULARITYENV_MPLCONFIGDIR="/group/interns2021026/mdoan/user_python/matp
 export SINGULARITYENV_TORCH_HOME="/group/interns2021026/mdoan/torch_home"
 export SINGULARITYENV_YOLOV5="/group/interns2021026/mdoan/yolov5_trash_recognition"
 export SINGULARITYENV_UTILS="/group/interns2021026/mdoan/yolov5_trash_recognition/utils/loggers"
+export SINGULARITYENV_RUNS="/group/interns2021026/mdoan/yolov5_trash_recognition/runs"
+export SINGULARITYENV_WANDB="/group/interns2021026/mdoan/yolov5_trash_recognition/wandb"
 
 SINGULARITYENV_CUDA=$CUDA_HOME
 time srun singularity exec --nv $MYGROUP/yolov5_trash_recognition/yolov5_run_container.sif python $MYGROUP/yolov5_trash_recognition/train.py --weights yolov5l.pt --imgsz 720 --data data/data-info.yaml --epochs 1000 --save-period 4
